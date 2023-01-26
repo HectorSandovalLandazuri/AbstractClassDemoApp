f360f3a56f4b723f277cbc84fcd944d4e384857d
Héctor Manuel Sandoval Landázuri <ingsan@live.com.mx>
2023-01-25 18:27:49 -0600

    Agregar archivos de proyecto.

---
+++ AbstractClassDemo/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+public abstract class InventoryItem
+{
+    public string ProductName { get; set; }
+    public int QuantityOnHand { get; set; }
+}
---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Book book=new Book();
+book.ProductName = "A Tale of two Cities";
+
+InventoryItem item = new Book();
+
+public abstract class Vehicle
+{
+    public string VIN { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public int YearManufactured { get; set; }
+}
+
+public class Car:Vehicle
+{
+    public int NumberOfWheels { get; set; } = 4;
+
+}
